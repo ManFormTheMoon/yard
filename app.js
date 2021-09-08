b99a76ea52723278d6ebff482ec93e1708e4f541
Ilya Shtanov <ilya.shtanov.2004@gmail.com>
2021-09-08 23:19:19 +0300

    first

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const config = require("config");
+
+const PORT = config.get("port");
+
+const app = express();
+
+app.use(express.json({ extended: "true" }));
+
+app.use("/api/auth", require("./routes/auth.routes"));
+
+app.listen(5000, () => console.log(`app startred on port ${PORT}`));
---
+++ routes/auth.routes.js
@@ -0,0 +1,51 @@
+const { Router } = require("express");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+var mysql = require("mysql2/promise");
+const router = Router();
+
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  database: "yard",
+  password: "ilya13524",
+  waitForConnections: true,
+});
+
+const getUser = async (login, password) => {
+  const result = await pool.query(
+    "select * from users where `login` = '" +
+      login +
+      "' AND `password` = '" +
+      password +
+      "';"
+  );
+  return result[0];
+};
+
+router.post("/login", async (req, res) => {
+  try {
+    const { login, password } = req.body;
+
+    const data = await getUser(login, password);
+
+    if (data.length == 0) {
+      return res.status(400).json({ message: "Неверные данные" });
+    }
+    console.log(data);
+    const token = jwt.sign(
+      { userId: data[0].id, userRole: "123" },
+      config.get("jwtSecret"),
+      { expiresIn: 5000 }
+    );
+    res.json({
+      token,
+      userId: data[0].id,
+      userRole: "123",
+      userName: data[0].first_name,
+      userLastName: data[0].last_name,
+    });
+  } catch (e) {}
+});
+
+module.exports = router;
